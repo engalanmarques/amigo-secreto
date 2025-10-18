c5909b4067c32573eda45fa68dd770e83a7d89bc
Alan Marques <eng.alanmarques@gmail.com>
2025-10-17 23:48:03 -0300

    adiciona amigo na lista e limpa o campo

---
+++ js/app.js
@@ -0,0 +1,10 @@
+// iniciar lista de amigos
+let listaDeAmigos = []
+
+function adicionar (){
+    // recupera o nome do amigo adicionado
+    let amigo = document.getElementById('nome-amigo').value;
+    listaDeAmigos.push(amigo);
+    // limpar campo
+    document.getElementById('nome-amigo').value = '';
+}
