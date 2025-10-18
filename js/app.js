2640fafb932b6d1a1d56d7a7c6bbc12724b66b22
Alan Marques <eng.alanmarques@gmail.com>
2025-10-18 00:44:26 -0300

    realizar o sorteio

--- js/app.js
+++ js/app.js
@@ -11,3 +11,30 @@ function adicionar (){
     document.getElementById('lista-amigos').textContent = listaDeAmigos;
 }
 
+function sortear(){
+    // copiar lista de amigos
+    let listaParaSorteio = listaDeAmigos;
+    let qtdAmigos = listaParaSorteio.length;
+    let qtdPareados = 0;
+    // inicia a lista com a ordem dos sorteados
+    let listaSorteio = [];
+    // sorteia um par para cada
+    while (qtdPareados < qtdAmigos){
+        let sorteado = parear(listaParaSorteio);
+        // verifica se houve repetição no sorteio, ou se sorteou a si mesmo
+        while (sorteado == listaParaSorteio[qtdPareados] || listaSorteio.includes(sorteado)){
+            sorteado = parear(listaParaSorteio);
+            console.log(`amigo1 ${listaParaSorteio[qtdPareados]} amigo2 ${sorteado}`)
+        }
+        // adiciona a lista dos sorteados
+        listaSorteio.push(sorteado);
+        qtdPareados++;
+    }
+}
+function parear(possiveisSorteados){
+    // sortear um amigo aleatorio entre o primeiro e o ultimo
+    let sorte = possiveisSorteados.length;
+    indiceSorteado = Math.floor(Math.random() * sorte);
+    let sorteado = possiveisSorteados[indiceSorteado];
+    return sorteado; 
+}
